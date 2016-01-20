d11d65e8ab72dec2e82beca6f43e5fab2da3834f
Asbee <Santtu_boman@hotmail.com>
2016-01-20 15:01:25 +0200

    Demo03

---
+++ Demo3KT5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3KT5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give 5 students information ");
+            Student[] student = new Student[5];
+
+            for (int i = 0;i < 5;)
+            {
+                
+                
+                    Console.Write("Student name ");
+                    student[i].Name = Console.ReadLine();
+                    
+                    Console.Write("Student Age ");
+                    student[i].Age = int.Parse(Console.ReadLine());
+
+                    Console.Write("Student School ");
+                    student[i].School = Console.ReadLine();
+
+                    Console.Write("Student Address ");
+                    student[i].Address = Console.ReadLine();
+
+
+                i++;
+            }
+
+            for (int i = 0;i<5;i++)
+            Console.WriteLine("Name = ", student[i].Name, student[i].Age);
+
+        }
+    }
+}
---
+++ Demo3KT5/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3KT5
+{
+    class Student
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string School { get; set; }
+    }
+}
